abe82ee3cadd985f112bbff7da870ce53bb39d42
Tomascod <tomas.kodydek@infis.cz>
2026-04-15 08:36:47 +0200

    cviceni

--- src/cviceni/Penezenka.java
+++ src/cviceni/Penezenka.java
@@ -4,13 +4,17 @@ public class Penezenka {
     private int zustatek;
 
     public void vlozCastku(int kolik) {
+        System.out.println("Vloženo "+kolik+" Kč");
         zustatek += kolik;
     }
 
     public void vyberCastku(int kolik){
         if (kolik>zustatek){
             System.out.println("Nemůžeš vybrat tolik, máš jen "+zustatek);
-        }else zustatek -= kolik;
+        }else{
+            System.out.println("Vybráno "+kolik+" Kč");
+            zustatek -= kolik;
+        }
     }
 
     public void vypisZustatek(){
