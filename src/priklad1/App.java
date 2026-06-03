cbf3ea87d29ac6106b4586302d3aa43f910fcebc
Tomascod <tomas.kodydek@infis.cz>
2026-06-03 08:06:21 +0200

    hotovo rozšířené zadání

--- src/priklad1/App.java
+++ src/priklad1/App.java
@@ -4,7 +4,14 @@ public class App {
     public static void main(String[] args) {
         Lampa.setmaxRozsviceno(2);
         Lampa l1 = new Lampa();
+        Lampa l2 = new Lampa();
+        Lampa l3 = new Lampa();
+
         l1.rozsvit();
+        l2.rozsvit();
+
+
+
 
 
 
